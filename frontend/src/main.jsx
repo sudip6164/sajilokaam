ad932bcbc58e5afefc7d260a7742a7be70637a9d
sudip6164 <sudippradhanadgj@gmail.com>
2025-11-16 20:55:35 +0545

    chore: remove test screen, app is working now

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import TestApp from './TestApp.jsx' // Uncomment to test if React works
 
-console.log('Main.jsx loaded, rendering App...')
-
-const rootElement = document.getElementById('root')
-if (!rootElement) {
-  console.error('Root element not found!')
-} else {
-  console.log('Root element found, creating root...')
-  const root = createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <App />
-      {/* <TestApp /> Uncomment to test */}
-    </StrictMode>
-  )
-  console.log('App rendered!')
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
