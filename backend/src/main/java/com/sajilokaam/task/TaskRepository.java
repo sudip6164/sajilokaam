bfb980f22241561b4b7aef4fa9575f58d98ec0e1
sudip6164 <sudippradhanadgj@gmail.com>
2025-11-16 18:36:33 +0545

    feat: update TaskRepository to eagerly load assignee and labels

--- backend/src/main/java/com/sajilokaam/task/TaskRepository.java
+++ backend/src/main/java/com/sajilokaam/task/TaskRepository.java
@@ -1,10 +1,15 @@
 package com.sajilokaam.task;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    @EntityGraph(attributePaths = {"assignee", "labels"})
     List<Task> findByProjectId(Long projectId);
+    
+    @EntityGraph(attributePaths = {"assignee", "labels"})
+    Optional<Task> findById(Long id);
 }
 
