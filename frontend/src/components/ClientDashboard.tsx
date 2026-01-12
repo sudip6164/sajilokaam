d182bc45619a264bfd2babc0aa15ca1784088987
sudip6164 <sudippradhanadgj@gmail.com>
2026-01-12 11:39:51 +0545

    fix: force zero left padding with inline style

--- frontend/src/components/ClientDashboard.tsx
+++ frontend/src/components/ClientDashboard.tsx
@@ -196,7 +196,7 @@ export function ClientDashboard() {
           </Sidebar>
 
           {/* Main Content - Scrollable */}
-          <main className="flex-1 overflow-y-auto ml-64 pt-16 pr-6 pb-6">
+          <main className="flex-1 overflow-y-auto ml-64 pt-16 pr-6 pb-6" style={{ paddingLeft: 0 }}>
             {renderContent()}
           </main>
         </SidebarProvider>
--- frontend/src/components/FreelancerDashboard.tsx
+++ frontend/src/components/FreelancerDashboard.tsx
@@ -91,7 +91,7 @@ export function FreelancerDashboard() {
           </Sidebar>
 
           {/* Main Content - Scrollable */}
-          <main className="flex-1 overflow-y-auto ml-64 pt-16 pr-6 pb-6">
+          <main className="flex-1 overflow-y-auto ml-64 pt-16 pr-6 pb-6" style={{ paddingLeft: 0 }}>
             {renderContent()}
           </main>
         </SidebarProvider>
