2750ea29daa0eb9e4c7ed5ff1fc379328ac3f460
sudip6164 <sudippradhanadgj@gmail.com>
2026-01-12 12:46:30 +0545

    fix: set left margin to 156px

--- frontend/src/components/ClientDashboard.tsx
+++ frontend/src/components/ClientDashboard.tsx
@@ -196,7 +196,7 @@ export function ClientDashboard() {
           </Sidebar>
 
           {/* Main Content - Scrollable */}
-          <main className="flex-1 overflow-y-auto pt-16 pr-6 pb-6 pl-3" style={{ marginLeft: '172px' }}>
+          <main className="flex-1 overflow-y-auto pt-16 pr-6 pb-6 pl-3" style={{ marginLeft: '156px' }}>
             {renderContent()}
           </main>
         </SidebarProvider>
--- frontend/src/components/FreelancerDashboard.tsx
+++ frontend/src/components/FreelancerDashboard.tsx
@@ -91,7 +91,7 @@ export function FreelancerDashboard() {
           </Sidebar>
 
           {/* Main Content - Scrollable */}
-          <main className="flex-1 overflow-y-auto pt-16 pr-6 pb-6 pl-3" style={{ marginLeft: '172px' }}>
+          <main className="flex-1 overflow-y-auto pt-16 pr-6 pb-6 pl-3" style={{ marginLeft: '156px' }}>
             {renderContent()}
           </main>
         </SidebarProvider>
